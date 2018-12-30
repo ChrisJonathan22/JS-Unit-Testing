6439469c383f767b1b4497cae4defb8d9850fd36
Chris Jonathan <christophereko@hotmail.fr>
2018-12-30 15:45:10 +0000

    The Integration Test is done and now on to The End To End test.

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('James', 40);
+    expect(text).toBe('James (40 years old)');
+});
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('James', 40);
     expect(text).toBe('James (40 years old)');
 });
